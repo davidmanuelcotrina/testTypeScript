712886d18efc71ac85564a9b88b24834257aba77
davidmanuelcotrina <davidmanuelcotrina@hotmail.com>
2024-01-02 07:06:03 -0500

    subir cambios merge

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,16 @@
 const userName = 'Bezael';
 const age = 33;
-<<<<<<< HEAD
-const pais = 'PERU'
+const sexo = 'Masculino';
+const pais = 'PERU';
 
-console.log(`hello lworld, my name is ${userName} i am ${age} year old. Pais de origen ${pais}`)
+console.log(`hello lworld, my name is ${userName} i am ${age} year old, sexo ${sexo}. Pais de origen ${pais}`)
 
-let numberValor: number = 11123;
+
+let numberValor: number = 123456;
 let textValor: string = "test01";
 let booleanValor: boolean = true;
 
 let arrayNumberValor: number[]=[11,2,3];
-=======
-const sexo = 'Masculino'
-
-console.log(`hello lworld, my name is ${userName} i am ${age} year old, sexo ${sexo}.`)
-
-
-let numberValor: number = 123456;
-let textValor: string = "test";
-let booleanValor: boolean = true;
-
-let arrayNumberValor: number[]=[1,2,3,4,5];
->>>>>>> main
 let arryStringValor: string[]= ["va1","va2","va3"];
 let arrayValor: any[]= ["va", 213];
 
