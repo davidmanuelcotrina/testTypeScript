0c3ff9a475fe0fbdac52e0eeb9cf2798d14c7bc0
davidmanuelcotrina <davidmanuelcotrina@hotmail.com>
2024-01-02 03:22:32 -0500

    cambios typescript

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+const userName = 'Bezael';
+const age = 33;
+console.log(`hello lworld, my name is ${userName} i am ${age} year old.`);
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+const userName = 'Bezael';
+const age = 33;
+
+console.log(`hello lworld, my name is ${userName} i am ${age} year old.`)
+
+let numberValor: number = 123;
+let textValor: string = "test";
+let booleanValor: boolean = true;
+
+let arrayNumberValor: number[]=[1,2,3];
+let arryStringValor: string[]= ["va1","va2","va3"];
+let arrayValor: any[]= ["va", 213];
+
+let tuplaValor: [string, number, boolean] = ["test", 232, true];
+let arrayTuplaValor: [string, number][] = [
+    ["test1", 23],
+    ["test2", 83],
+    ["test3", 53]
+];
+
+let variosTiposValor: string | number | null;
+variosTiposValor = "";
