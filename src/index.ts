c072644f5b933887547a7c8b539324cbd4357ff9
davidmanuelcotrina <davidmanuelcotrina@hotmail.com>
2024-01-02 05:39:08 -0500

    cambios msi

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 const userName = 'Bezael';
 const age = 33;
+const pais = 'PERU'
 
-console.log(`hello lworld, my name is ${userName} i am ${age} year old.`)
+console.log(`hello lworld, my name is ${userName} i am ${age} year old. Pais de origen ${pais}`)
 
-let numberValor: number = 123;
-let textValor: string = "test";
+let numberValor: number = 11123;
+let textValor: string = "test01";
 let booleanValor: boolean = true;
 
-let arrayNumberValor: number[]=[1,2,3];
+let arrayNumberValor: number[]=[11,2,3];
 let arryStringValor: string[]= ["va1","va2","va3"];
 let arrayValor: any[]= ["va", 213];
 
 let tuplaValor: [string, number, boolean] = ["test", 232, true];
 let arrayTuplaValor: [string, number][] = [
-    ["test1", 23],
     ["test2", 83],
     ["test3", 53]
 ];
